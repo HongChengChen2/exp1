5a7740ee31d0c118d4df092b4d447bd7f3bbac91
hcc <741472594@qq.com>
2021-03-08 14:33:08 +0800

    2

--- imagenet/weight-level/prune.py
+++ imagenet/weight-level/prune.py
@@ -76,6 +76,34 @@ transform = transforms.Compose([
     transforms.Normalize(mean = mean, std = std),
     ])
 
+def default_loader(path):
+    return Image.open(path).convert('RGB')
+
+class MyDataset(Dataset):
+    def __init__(self, txt, transform=None, target_transform=None, loader=default_loader):
+        fh = open(txt, 'r')
+        imgs = []
+        for line in fh:
+            line = line.rstrip()
+            line = line.strip('\n')
+            line = line.rstrip()
+            words = line.split()
+            imgs.append((words[0],int(words[1])))
+        self.imgs = imgs
+        self.transform = transform
+        self.target_transform = target_transform
+        self.loader = loader
+
+    def __getitem__(self, index):
+        fn, label = self.imgs[index]
+        img = self.loader(fn)
+        if self.transform is not None:
+            img = self.transform(img)
+        return img,label
+
+    def __len__(self):
+        return len(self.imgs)
+        
 def main():
     global args, best_prec1
     args = parser.parse_args()
