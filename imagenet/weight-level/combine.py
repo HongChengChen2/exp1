cce4c000e555ce73caf92c75389ef7cf3d099bc6
hcc <741472594@qq.com>
2021-03-18 10:44:41 +0800

    15

--- imagenet/weight-level/combine.py
+++ imagenet/weight-level/combine.py
@@ -186,7 +186,7 @@ def main():
         model_2.load_state_dict(checkpoint2) #cat dog
 
 
-    val_dataset = MyDataset(txt=args.data+'dataset-val.txt', transform=transform)
+    val_dataset = MyDataset(txt=args.data+'dataset-val2.txt', transform=transform)
     val_loader = torch.utils.data.DataLoader(val_dataset , batch_size=args.batch_size, shuffle=False,
         num_workers=args.workers, pin_memory=True)
 
