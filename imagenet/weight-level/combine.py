712affce6f3e2a6debd7fca5aca7536e936958d4
hcc <741472594@qq.com>
2021-03-29 21:46:17 +0800

    3

--- imagenet/weight-level/combine.py
+++ imagenet/weight-level/combine.py
@@ -302,7 +302,8 @@ def accuracy(output_1,output_2, target, topk=(1,)):
         #print("number1.shape[1]",number1.shape[1])
         for a in range(0,number1.shape[0]):
             #gap_0 = number1[a][0] - number1[a][1]
-            gap = number1[a][0]
+            gap = random.random(0,1)
+            #gap = number1[a][0]
             #gap = gap_0.item()
             #print("gap:",gap)
             #print("a:",a)
